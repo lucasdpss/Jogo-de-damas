e96ecaa8f5fd82fc109f21662cd2337ba6c3a102
lucasdpss <lucasdepaula_s@outlook.com>
2020-04-14 15:00:11 -0300

    estrutura inicial

---
+++ src/jogo_de_damas/Bolinha.java
@@ -0,0 +1,9 @@
+package jogo_de_damas;
+
+public class Bolinha {
+	private char caractere = 'P';
+	
+	public char getCaractere(){
+		return caractere;
+	}
+}
---
+++ src/jogo_de_damas/Main.java
@@ -0,0 +1,23 @@
+package jogo_de_damas;
+
+public class Main {
+
+	public static void main(String[] args) {
+		CSVReader csv = new CSVReader();
+		csv.setDataSource("src\\solution.csv");
+		String commands[] = csv.requestCommands();
+		
+		Tabuleiro t = new Tabuleiro();
+		
+		t.mostrar();
+		
+		for(int i=0;i < commands.length;i++) {
+			System.out.println("Source: "+commands[i].charAt(0)+commands[i].charAt(1));
+			System.out.println("target: "+commands[i].charAt(3)+commands[i].charAt(4));
+			t.mover_peca(commands[i]);
+			t.mostrar();
+		}
+
+	}
+
+}
